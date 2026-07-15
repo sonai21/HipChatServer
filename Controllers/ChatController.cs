233d5c95a0ae32b39ad26ce9f1a36a610b9bde03
alangkrita <alangkrita.sarkar@seaker.ai>
2026-07-15 23:25:01 +0530

    Add project files.

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace HipChatServer.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ChatController : ControllerBase
+{
+    private readonly Kernel _kernel;
+    public ChatController(Kernel kernel)
+    {
+        _kernel = kernel;
+    }
+
+    [HttpPost("askQuestion")]
+    public async Task<IActionResult> AskQuestion([FromBody] string question)
+    {
+        if (string.IsNullOrWhiteSpace(question))
+        {
+            return BadRequest("Prompt can not be empty!");
+        }
+
+        try
+        {
+            //retive chat completion service from kernel
+            var chatCompletionService = _kernel.GetRequiredService<IChatCompletionService>();
+
+            //create chat history with previous promt
+            var chatHistory = new ChatHistory("AI Generated");
+            chatHistory.AddUserMessage(question);
+
+            //sending request to LM Studio
+            var response = await chatCompletionService.GetChatMessageContentAsync(chatHistory, kernel: _kernel);
+
+            return Ok(new { Response = response.Content});
+        }
+
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"An error occurred: {ex.Message}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.SemanticKernel;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//integrating LM Studio with semantic kernel
+var endpoint = new Uri("http://127.0.0.1:1234/v1");
+var model = "qwen/qwen3-1.7b";
+var apiKey = "guest";
+
+var kernelBuilder = Kernel.CreateBuilder().AddOpenAIChatCompletion(model, endpoint, apiKey);
+var kernel = kernelBuilder.Build();
+builder.Services.AddSingleton(kernel);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
