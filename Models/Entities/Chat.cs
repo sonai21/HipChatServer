5d81c6a2c6ee64577035aec43dd6d4c009a73f61
alangkrita <alangkrita.sarkar@seaker.ai>
2026-07-16 15:53:46 +0530

    Define models

---
+++ Models/Entities/Chat.cs
@@ -0,0 +1,10 @@
+﻿namespace HipChatServer.Models.Entities;
+
+public class Chat
+{
+    public Guid Id { get; set; }
+    public string? Title { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public ICollection<Message?> Messages { get; set; }
+}
---
+++ Models/Entities/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace HipChatServer.Models.Entities;
+
+public class Message
+{
+    public Guid Id { get; set; }
+    public Guid ChatId { get; set; }
+
+    public string? Role { get; set; }
+    public string? Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Chat? Chat { get; set; }
+}
